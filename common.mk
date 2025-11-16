f3d52912093967c7894e3852434309c1f6ee36a5
LuK1337 <priv.luk@gmail.com>
2025-11-16 10:56:33 +0100

    sm7125-common: Opt out of speaker_layout_channel_mask field
    Required for ABI compatibility with audio HAL compiled before 15 QPR2.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
    Change-Id: Ic910f0634ee0707c21dad77f5264fd89fb33f1c0

--- common.mk
+++ common.mk
@@ -28,8 +28,7 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 $(call soong_config_set,surfaceflinger,udfps_lib,//$(LOCAL_PATH):libudfps_extension.realme_sm7125)
 
 # speaker layout channel mask
-$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
-
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BOARD_PLATFORM := atoll
